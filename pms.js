0252bc3fb8886334169ecc92596f34443c19cb0b
Thuta <thutaineum@live.co.uk>
2025-03-18 18:28:03 +0000

    login and signup pages added

--- pms.js
+++ pms.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileSystem = require('fs');
+const { title } = require('process');
 
 
 class PMS {
@@ -17,12 +18,53 @@ class PMS {
 
     this.pms.get('/', (req, res) => {
       const indexContent = {
-        "title": "Test Title",
-        "headerOne": "Test Header"
+        title: "ParkName",
+        siteName: "ParkName",
+        home: "Home",
+        about: "About",
+        services: "Services",
+        contact: "Contact",
+        login: "Login",
+        signUp: "Sign Up",
+        heroHeader: "Welcome to our Website",
+
+        footerText: "2025 Simple starter Website"
       }
 
       res.render('index', indexContent)
 
+    });
+
+    this.pms.get('/signup', (req, res) => {
+      const signupContent = {
+        title: "ParkName",
+        siteName: "ParkName",
+        home: "Home",
+        about: "About",
+        services: "Services",
+        contact: "Contact",
+        login: "Login",
+        signUp: "Sign Up",
+        footerText: "2025 Simple starter website"
+      }
+      
+      res.render('signup', signupContent)
+    });
+
+    this.pms.get('/login', (req, res) => {
+      const loginContent = {
+        title: "ParkName",
+        siteName: "ParkName",
+        home: "Home",
+        about: "About",
+        services: "Services",
+        contact: "Contact",
+        login: "Login",
+        signUp: "Sign Up",
+        footerText: "2025 Simple starter website"
+      }
+
+      res.render('login', loginContent)
     });
 
     this.pms.listen(this.PORT, () => {
