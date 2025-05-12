b44ecdb6f2e01cfed073b97fcfdfc78361597c03
Aiden Ayers <azurefalctis@proton.me>
2025-05-12 14:58:49 +0100

    Added user ID cookie for the reservation functionality

--- controllers/ParkingRequestController.js
+++ controllers/ParkingRequestController.js
@@ -1,23 +1,45 @@
 const ParkingRequest = require('../models/parkingrequest');
 const DriverUser = require('../models/driveruser');
 
+
+String.prototype.toObjectId = function() {
+  var ObjectId = (require('mongoose').Types.ObjectId);
+  return new ObjectId(this.toString());
+};
+
 // Create parking request
 exports.makeReservation = async (req, res) => {
-    const { driverID, arrivalTime, departureTime } = req.body;
+    const { location, arrivalTime, departureTime, registration } = req.body;
 
-    if (!driverID || !arrivalTime || !departureTime) {
-        return res.status(400).send('Missing required fields');
-    }
+    console.log(location, arrivalTime, departureTime, registration);
+
+    console.log(req.cookies)
+  
+
+    driverID = req.cookies.user_id;
+    console.log(driverID);
+
+    
+
+    driver = await DriverUser.findById(driverID).exec();
+    console.log(driver.email);
+
+    // if (!driverID || !arrivalTime || !departureTime) {
+    //     return res.status(400).send('Missing required fields');
+    // }
 
     try {
         const parkingRequest = new ParkingRequest({
-            driver: driverID,
-            arrivalTime,
-            departureTime,
+            driver: driver,
+            arrivalTime: arrivalTime,
+            departureTime: departureTime,
         });
         await parkingRequest.save();
 
-        res.render('viewParkingRequests')
+        const requestID = parkingRequest.get("_id");
+        res.cookie("requestID", requestID);  
+
+        //res.render('viewParkingRequests')
     } catch (err) {
         return res.status(500).send(err.message);
     }
