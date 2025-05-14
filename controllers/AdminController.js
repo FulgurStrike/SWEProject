d7490fe1e8a0731d72d26477598065012f9e0240
Hughjmillard <hughjmillard@gmail.com>
2025-05-14 17:10:01 +0100

    fix bug

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,20 +1,5 @@
+const ParkingRequest = require('../models/parkingrequest');
 
-// View parking requests for a user
-exports.viewUserParkingRequests = async (req, res) => {
-    const {userID} = req.params;
-
-    try {
-        const parkingRequests = await ParkingRequest.find({driver: userID})
-            .populate('parkingSpace')
-            .populate('driver');
-        if (parkingRequests.length === 0) {
-            return res.status(404).send('No parking requests found for this user');
-        }
-        res.render('adminDashboard/viewUserParkingRequests', {parkingRequests, userID});
-    } catch (error) {
-        return res.status(500).send(err.message);
-    }
-}
 
 // Approve parking request
 exports.approveParkingRequest = async (req, res) => {
